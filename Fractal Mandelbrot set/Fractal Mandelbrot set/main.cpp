7462463ced1d690faeb388880db4d75bfdf8271e
Osama M. Afifi <osama.egt@gmail.com>
2015-05-13 00:21:50 +0200

    Added the infinite zooming feature

--- Fractal Mandelbrot set/Fractal Mandelbrot set/main.cpp
+++ Fractal Mandelbrot set/Fractal Mandelbrot set/main.cpp
@@ -27,8 +27,8 @@ int num_of_processes;
 #define minY -2
 #define maxX 2
 #define maxY 2
-#define maxZoomLevel 10
-#define minZoomLevel 0.1
+//#define maxZoomLevel 100000
+//#define minZoomLevel 0.1
 
 int *fractal_area_depth;
 void InitGraphics(int argc, char *argv[]);
@@ -41,48 +41,34 @@ int DEPTH_THRESHOLD=50;
 void keyPressed(unsigned char key, int x, int y)
 {
 	if(key == '+')
-	{
 		zoomLevel += 0.1 * zoomLevel;
-	}
-	else if(key == '-')
-	{
+	if(key == '-')
 		zoomLevel -= 0.1 * zoomLevel;
-	}
-
+	if(key == 'r')
+		DEPTH_THRESHOLD++;
+	if(key == 't')
+		DEPTH_THRESHOLD--;
 	//validate that the zoomLevel is within the range
-	zoomLevel = min(zoomLevel, maxZoomLevel);
-	zoomLevel = max(zoomLevel, minZoomLevel);
-	printf("Zoom Level = %d\n", zoomLevel);
+	//zoomLevel = min(zoomLevel, maxZoomLevel);
+	//zoomLevel = max(zoomLevel, minZoomLevel);
+	
+	//double newCenterX = centerX, newCenterY = centerY;
+	if(key == 'w')
+		centerY += 0.1 * (1 / zoomLevel);
+	if(key == 's')
+		centerY -= 0.1 * (1 / zoomLevel);
+	if(key == 'a')
+		centerX -= 0.1 * (1 / zoomLevel);	
+	if(key == 'd')
+		centerX += 0.1 * (1 / zoomLevel);
+
+	//if(newCenterX - 2 * (1 / zoomLevel) >= minX && newCenterX + 2 * (1 / zoomLevel) <= maxX && newCenterY - 2 * (1 / zoomLevel) >= minY && newCenterY + 2 * (1 / zoomLevel) <= maxY)
+	//{
+	//	centerX = newCenterX;
+	//	centerY = newCenterY;
+	//}
+
 }
-
-void specialKeyPressed(int key, int x, int y)
-{
-	double newCenterX = centerX, newCenterY = centerY;
-	if(key == GLUT_KEY_UP)
-	{
-		newCenterY += 0.1 * (1 / zoomLevel);
-	}
-	else if(key == GLUT_KEY_DOWN)
-	{
-		newCenterY -= 0.1 * (1 / zoomLevel);
-	}
-	else if(key == GLUT_KEY_LEFT)
-	{
-		newCenterX -= 0.1 * (1 / zoomLevel);
-	}
-	else if(key == GLUT_KEY_RIGHT)
-	{
-		newCenterX += 0.1 * (1 / zoomLevel);
-	}
-
-	printf("special key pressed\n");
-	//validate that the window is within the range
-	if(newCenterX - 2 * (1 / zoomLevel) >= minX && newCenterX + 2 * (1 / zoomLevel) <= maxX && newCenterY - 2 * (1 / zoomLevel) >= minY && newCenterY + 2 * (1 / zoomLevel) <= maxY)
-	{
-		centerX = newCenterX;
-		centerY = newCenterY;
-	}
-} 
 
 
 int f(complex<double>c)
@@ -110,10 +96,14 @@ void SetTransformations()
 void updateFractals(int machineID, int *slave_buffer)
 {
 	double params[4]; 
-	// 0: TOP_LEFT_X
-	// 1: TOP_LEFT_Y
+	// 0: CENTERX
+	// 1: CENTERY
 	// 2: ZOOM_LVL
 	// 3: DEPTH_THRESHOLD
+	complex<double> center(params[0],params[1]);
+	double zoomLevel=params[2];
+	int depth_thres=params[3];
+
 	MPI_Recv(&params, 4, MPI_DOUBLE, 0, MPI_ANY_TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
 	int fraction_size = (WINDOW_HEIGHT*WINDOW_WIDTH)/(num_of_processes-1);
 	int start_pos = (machineID-1) * fraction_size;
@@ -123,8 +113,8 @@ void updateFractals(int machineID, int *slave_buffer)
 	{
 		int y = i/WINDOW_WIDTH;
 		int x = i%WINDOW_WIDTH;
-		int depth = f(complex<double>((1 / zoomLevel) * (x-WINDOW_WIDTH/2)/(WINDOW_WIDTH/4.0), (1 / zoomLevel) * (y-WINDOW_HEIGHT/2)/(WINDOW_HEIGHT/4.0)));
-		slave_buffer[i-start_pos]= (depth<DEPTH_THRESHOLD) ? depth : 0;
+		int depth = f(center+complex<double>((1 / zoomLevel) * (x-WINDOW_WIDTH/2)/(WINDOW_WIDTH/4.0), (1 / zoomLevel) * (y-WINDOW_HEIGHT/2)/(WINDOW_HEIGHT/4.0)));
+		slave_buffer[i-start_pos]= (depth<depth_thres) ? depth : 0;
 	}
 	MPI_Send(slave_buffer, fraction_size, MPI_INT, 0, machineID, MPI_COMM_WORLD);
 }
@@ -133,11 +123,11 @@ void updateFractals(int machineID, int *slave_buffer)
 void OnDisplay() 
 {
 	//Sends a starting signal (current area of interest)to the slaves for the current frame computation
-	double params[4] = {-1,-1,-1,-1};
-	// 0: TOP_LEFT_X
-	// 1: TOP_LEFT_Y
-	// 2: ZOOM_LVL
-	// 3: DEPTH_THRESHOLD
+	double params[4];
+	params[0] = centerX;// 0: CENTERX
+	params[1] = centerY;// 1: CENTERY
+	params[2] = zoomLevel;// 2: ZOOM_LVL
+	params[3] = DEPTH_THRESHOLD;// 3: DEPTH_THRESHOLD
 	double start_time = omp_get_wtime();
 
 	for(int i= 1 ; i<num_of_processes ; i++)
@@ -153,7 +143,7 @@ void OnDisplay()
 		MPI_Recv((fractal_area_depth+(i-1)*fraction_size), fraction_size, MPI_INT, i, MPI_ANY_TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
 
 	char fpsString[128];
-	sprintf(fpsString, "%f fps", 1/(omp_get_wtime()-start_time));
+	sprintf(fpsString, "%f fps, Zoom Level:%fx, Center-x=%f, Center-Y=%f, DepthResolution=%d", 1/(omp_get_wtime()-start_time), zoomLevel , centerX , centerY,DEPTH_THRESHOLD);
 	glutSetWindowTitle(fpsString);
 
 	for(int i = 0 ; i<WINDOW_HEIGHT ; i++)
@@ -179,7 +169,6 @@ void InitGraphics(int argc, char *argv[])
 	glutCreateWindow("Parallel Mandelbrot Set Fractals Visualization");
 	glutDisplayFunc(OnDisplay);
 	glutKeyboardFunc(keyPressed);
-	glutSpecialFunc(specialKeyPressed);
 	printf("funcitons registered\n");
 	glutMainLoop();
 }
